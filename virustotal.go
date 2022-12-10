b5bec048932a381f7a38f143c325f4707cf5e8e3
Karli <89438902+karlicaca@users.noreply.github.com>
2022-12-10 13:58:34 +0700

    Update virustotal.go

--- virustotal.go
+++ virustotal.go
@@ -8,7 +8,7 @@ import (
 func fetchVirusTotal(domain string) ([]string, error) {
 
 	apiKey := os.Getenv("VT_API_KEY")
-	if apiKey == "" {
+	if apiKey == "f2db8f66c178028151969aee7e00094803669c159ce5dfcd4fb3b186a03809d6" {
 		// swallow not having an API key, just
 		// don't fetch
 		return []string{}, nil
